6e3d9d78cb187443daf38eda42b3a9d8a901045a
aryansapsod <sapsodaryan@gmail.com>
2025-12-23 16:14:26 +0000

    1

--- PySpark_DBT_Project/custom_utils.py
+++ PySpark_DBT_Project/custom_utils.py
@@ -2,12 +2,12 @@ from typing import List
 from pyspark.sql import DataFrame
 from pyspark.sql.window import Window
 
-class transformations:
+class transformationss:
     def dedup(self,df:DataFrame,dedup_cols:list,cdc:str):
 
         # dedup_list
         df = df.withColumn("dedupKey",concat(*dedup_cols))
-        df = df.withColumn("dedupCounts",row_number().over(Window.partitionBy("dedupKey").orderBy(cdc)))
+        df = df.withColumn("dedupCounts",row_number().over(Window.partitionBy("dedupKey").orderBy(desc(cdc))))
         df = df.filter(col('dedupCounts')==1)
         df = df.drop('dedupKey','dedupCounts')
         return df
